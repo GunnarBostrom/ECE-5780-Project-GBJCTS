895dfcb1133306d55db126b88b6663b0ba153da5
Trenton Stratton <trenton.stratton@gmail.com>
2026-04-03 08:54:53 -0600

    Stub out LiDAR driver

--- inc/lidar.h
+++ inc/lidar.h
@@ -7,10 +7,34 @@ STM VL53L1X
 #ifndef LIDAR_H
 #define LIDAR_H
 
-#include "config.h"
-#include "stm32f0xx_hal.h" // Needed for HAL types if you use them later
+#include <stdint.h>
+#include "stm32f0xx_hal.h" // probably not needed
+
+// just what we need
+typedef struct {
+    uint16_t range_mm;      // range in millimeters
+    uint32_t range_sigma;   // signal standard deviation (divide by 65536 for real value)
+    uint8_t range_status;   // signal validity           (0 = valid)
+    uint32_t signal_rate;   // signal strength           (divide by 65536 for real value)
+    uint32_t ambient_rate;  // background interference   (divide by 65536 for real value)
+} LIDAR_t;
+
+// all data peripheral provides
+typedef struct {
+    uint8_t  stream_count;
+    uint32_t signal_rate;   // divide by 65536 for real value
+    uint32_t ambient_rate;  // divide by 65536 for real value
+    uint16_t spad_count;    // divide by 256 for real value
+    uint32_t sigma_mm;      // divide by 65536 for real value
+    int16_t  range_mm;      // distance in mm
+    uint8_t  range_status;  // 0 = valid
+} TOF_t;
 
 void lidar_init(void);
-void lidar_read(void);
+void lidar_read_range(void);
+void lidar_read_sigma(void);
+void lidar_read_status(void);
+void lidar_read_strength(void);
+void lidar_read_interference(void);
 
 #endif // LIDAR_H
--- src/lidar.c
+++ src/lidar.c
@@ -1,22 +1,44 @@
 /**
 LiDAR
+
 STM VL53L1X
+Can operate at "up to 400 kHz".
 
-VL53L1X can operate at "up to 400 kHz".
+Data peripheral provides:
+    - range
+    - range standard deviation
+    - range validity
+    - signal strength
+    - ambient interference
+    - reading count
+    - active SPAD count
 
+I2C slave address: 0x52
 WHO_AM_I register: 0x010F
 Device ID: 0xEA
-I2C slave address: 0x52
 */
 
+#include "config.h"
 #include "lidar.h"
+
+// define device
+// defien output regs
+// define config regs
+// define config vals
 
 
 #if USE_LIDAR // use REAL hardware
-void lidar_init(void) { /* HAL_I2C_Init... */ }
-void lidar_read(void) { /* HAL_I2C_Receive... */ }
+
+void lidar_init(void) {}
+void lidar_read_range(void) {}
+void lidar_read_status(void) {}
+void lidar_read_strength(void) {}
+void lidar_read_interference(void) {}
 
 #else //use FAKE hardware
 void lidar_init(void) { /* do nothing */ }
-void lidar_read(void) { /* do nothing */ }
+void lidar_read_range(void) { /* do nothing */ }
+void lidar_read_status(void) { /* do nothing */ }
+void lidar_read_strength(void) { /* do nothing */ }
+void lidar_read_interference(void) { /* do nothing */ }
 #endif
