163ea823db49ea31f8ec4d92400288f4e8be3e2f
Trenton Stratton <trenton.stratton@gmail.com>
2026-04-03 01:44:11 -0600

    Preliminary file prep

--- inc/lidar.h
+++ inc/lidar.h
@@ -1,9 +1,16 @@
+/**
+LiDAR
+STM VL53L1X
+
+*/
+
 #ifndef LIDAR_H
 #define LIDAR_H
 
+#include "config.h"
 #include "stm32f0xx_hal.h" // Needed for HAL types if you use them later
 
 void lidar_init(void);
 void lidar_read(void);
 
-#endif
+#endif // LIDAR_H
