f9f02526e237b6f5bf4b05d9b32745c13ff0822f
POACH3 <140459864+POACH3@users.noreply.github.com>
2026-04-03 21:08:29 -0600

    Resolved conflicts

--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
+/**
+ * Flight Controller
+ * STM32F072x Discovery Board
+ */
+
 #include "main.h"
 #include "stm32f072xb.h"
 #include "stm32f0xx_hal.h"
 #include "stm32f0xx_hal_gpio.h"
 #include "stm32f0xx_it.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <sys/_intsup.h>
 #include <sys/types.h>
 #include "motor.h"
 #include "config.h"
+#include "i2c.h"
 #include "imu.h"
 #include "lidar.h"
 #include "radio.h"
@@ -15,114 +22,163 @@
 void Error_Handler(void);
 void SystemClock_Config(void);
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+void LED_init(void);
 
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-  /* Configure the system clock */
-  SystemClock_Config();
+/* –––––––––– globals –––––––––– */
+IMU_t lsm6ds3;
+//LIDAR_t vl52l1x;
 
-  imu_init();
-  radio_init();
-  lidar_init();
 
-  // LED INIT for main loop heartbeat
-static void LED_init(void)
-{
-    GPIO_InitTypeDef init = {0};
 
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-
-    // PC8 = orange, PC9 = green
-    init.Pin = GPIO_PIN_8 | GPIO_PIN_9;
-    init.Mode = GPIO_MODE_OUTPUT_PP;
-    init.Pull = GPIO_NOPULL;
-    init.Speed = GPIO_SPEED_FREQ_LOW;
-
-  while(1){
-    
-    imu_read();
-    radio_read();
-    lidar_read();
-    HAL_GPIO_Init(GPIOC, &init);
-
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_RESET);
-}
-
-int main(void)
-{
-  /*loop structure:*/
-  //read sensors
-  //compute error
-  //run PID
-  //mix motors
-  //update PWM
-  //imu_init();
+int main(void) {
   HAL_Init();
   SystemClock_Config();
+
+  // semihosting to redirect printf for debugging (must be first in main)
+  // extern void initialise_monitor_handles(void);
+  // initialise_monitor_handles();
+
   LED_init();
+  
+
+  // I2C peripheral initialization
+  i2c_init(400);
+
+  imu_init(&lsm6ds3, 0x6A); // i2c addr: 0x6A, 0x6B is default
+  lidar_init();             // i2c addr: 0x52
+
+
+  // UART peripheral initialization
+  radio_init();
+
+
+  // PWM peripheral initialization
   motor_init();
 
   uint32_t last_heartbeat = HAL_GetTick();
   uint32_t last_toggle = HAL_GetTick();
-
   uint8_t motor_state = 0;
 
   // Arm all ESCs at minimum throttle
   motor_set_all(1000);
   // motor_set_individual(1000, 1000, 1000, 1000);
-   HAL_Delay(8000);
+  HAL_Delay(8000);
 
   // Hold Motors 1-4 at low throttle
   // Note motor minimum value for all 4 motors to spin at min throttle is 1200
   // motor_set_individual(1200, 1200, 1200, 1200);
   motor_set_all(1200);
+  
+  
 
-  while (1)
-  {
-      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // heartbeat
-      HAL_Delay(500);
-    }
+  /*
+   CONTROL LOOP STRUCTURE
+      - read sensors
+      - compute error
+      - run PID
+      - mix motors
+      - update PWM
+  */
+  while(1) {
+    // need to think about precedence and data frequency
+
+    
+    imu_read(&lsm6ds3); // highest priority - interrupt with flag
+    // update motors as soon as IMU data ready
+
+    radio_read();       // medium priority - interrupt with flag
+
+    lidar_read();       // lowest priority - polls
+
+    
+
+
+
+
+
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // heartbeat
+    HAL_Delay(500);
+
+
+
+    /* tentative loop structure */
+
+    // if (imu_ready) {
+    //   imu_ready = 0;
+    //   imu_read(&lsm6ds3);
+
+    //   lidar_counter++;
+    //   if (lidar_counter > some_threshold){
+    //     lidar_counter = 0;
+    //     lidar_read(&vl53l1x);
+    //   }
+
+    //   run_pid();
+    //   update_motors();
+    // }
+
+    // if (radio_ready) {
+    //   radio_ready = 0;
+    //   radio_read();
+    // }
+
+
+  }
+  // nothing should be in main() below the control loop
 }
 
-  // END DRONE CODE
 
-  // // BEGIN BLINKY CODE
 
-  // /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  // HAL_Init();
-  // /* Configure the system clock */
-  // SystemClock_Config();
+/**
+ * @brief LED initialization for heartbeat and debug.
+ * 
+ * PC6 - red
+ * PC7 - blue
+ * PC8 - orange
+ * PC9 - green
+ */
+static void LED_init(void) {
+  // GPIO_InitTypeDef init = {0};
 
-  // RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-  // (void)RCC->AHBENR;
+  // __HAL_RCC_GPIOC_CLK_ENABLE();
+
   
-  // GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
-  //                             GPIO_MODE_OUTPUT_PP,
-  //                             GPIO_SPEED_FREQ_LOW,
-  //                             GPIO_NOPULL};
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+  (void)RCC->AHBENR;
+  
+  GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+                              GPIO_MODE_OUTPUT_PP,
+                              GPIO_SPEED_FREQ_LOW,
+                              GPIO_NOPULL};
 
-  // HAL_GPIO_Init(GPIOC, &initStr);
-  // HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_SET);
-
+  HAL_GPIO_Init(GPIOC, &initStr);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_SET); // leds on
+  HAL_Delay(1000);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // red off
+  HAL_Delay(500);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // green off
+  HAL_Delay(500);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7); // blue off
+  HAL_Delay(500);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8); // orange off
+  HAL_Delay(500);
 
   // while (1){
   //   HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
   //   HAL_Delay(500);
   // }
-//}
+
+}
+
+
 
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
@@ -133,8 +189,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
@@ -146,8 +201,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
     Error_Handler();
   }
 }
@@ -156,8 +210,7 @@ void SystemClock_Config(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
+void Error_Handler(void) {
   /* User can add their own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
@@ -173,8 +226,7 @@ void Error_Handler(void)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+void assert_failed(uint8_t *file, uint32_t line) {
   /* User can add their own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 }
