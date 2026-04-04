43c4b66fd1f156362856ff8ead544f888c4c8b0a
Trenton Stratton <trenton.stratton@gmail.com>
2026-04-04 10:12:30 -0600

    Add lidar_init and defines

--- inc/lidar.h
+++ inc/lidar.h
@@ -12,6 +12,7 @@ STM VL53L1X
 
 // just what we need
 typedef struct {
+    uint8_t slave_addr;
     uint16_t range_mm;      // range in millimeters
     uint32_t range_sigma;   // signal standard deviation (divide by 65536 for real value)
     uint8_t range_status;   // signal validity           (0 = valid)
@@ -30,11 +31,11 @@ typedef struct {
     uint8_t  range_status;  // 0 = valid
 } TOF_t;
 
-void lidar_init(void);
-void lidar_read_range(void);
-void lidar_read_sigma(void);
-void lidar_read_status(void);
-void lidar_read_strength(void);
-void lidar_read_interference(void);
+void lidar_init(LIDAR_t *lidar, uint8_t slave_addr);
+void lidar_read_range(LIDAR_t *lidar);
+void lidar_read_sigma(LIDAR_t *lidar);
+void lidar_read_status(LIDAR_t *lidar);
+void lidar_read_strength(LIDAR_t *lidar);
+void lidar_read_interference(LIDAR_t *lidar);
 
 #endif // LIDAR_H
