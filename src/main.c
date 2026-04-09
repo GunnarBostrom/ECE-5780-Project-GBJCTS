9f904fa7b85ce0e16aaee2837427d2d502951bb9
Trenton Stratton <trenton.stratton@gmail.com>
2026-04-08 18:29:01 -0600

    Fix bug preventing I2C Fast-mode in i2c_bus_reset()

--- src/main.c
+++ src/main.c
@@ -45,30 +45,30 @@ int main(void) {
   // I2C peripheral initialization
   i2c_init(400);
 
-  imu_init(&lsm6ds3, 0x6B);    // i2c addr: 0x6A, 0x6B is default
-  lidar_init(&vl53l1x, 0x52); // i2c addr: 0x52
+  imu_init(&lsm6ds3, 0x6B);     // i2c addr: 0x6B
+  // lidar_init(&vl53l1x, 0x52); // i2c addr: 0x52
 
 
-  // UART peripheral initialization
-  radio_init();
+  // // UART peripheral initialization
+  // radio_init();
 
 
-  // PWM peripheral initialization
-  motor_init();
+  // // PWM peripheral initialization
+  // motor_init();
 
-  uint32_t last_heartbeat = HAL_GetTick();
-  uint32_t last_toggle = HAL_GetTick();
-  uint8_t motor_state = 0;
+  // uint32_t last_heartbeat = HAL_GetTick();
+  // uint32_t last_toggle = HAL_GetTick();
+  // uint8_t motor_state = 0;
 
-  // Arm all ESCs at minimum throttle
-  motor_set_all(1000);
-  motor_set_individual(1000, 1000, 1000, 1000);
-  HAL_Delay(8000);
+  // // Arm all ESCs at minimum throttle
+  // motor_set_all(1000);
+  // motor_set_individual(1000, 1000, 1000, 1000);
+  // HAL_Delay(8000);
 
-  // Hold Motors 1-4 at low throttle
-  // Note motor minimum value for all 4 motors to spin at min throttle is 1200
-  motor_set_individual(1200, 1200, 1200, 1200);
-  motor_set_all(1200);
+  // // Hold Motors 1-4 at low throttle
+  // // Note motor minimum value for all 4 motors to spin at min throttle is 1200
+  // motor_set_individual(1200, 1200, 1200, 1200);
+  // motor_set_all(1200);
   
   
 
@@ -87,9 +87,9 @@ int main(void) {
     imu_read(&lsm6ds3); // highest priority - interrupt with flag
     // update motors as soon as IMU data ready
 
-    radio_read();       // medium priority - interrupt with flag
+    // radio_read();       // medium priority - interrupt with flag
 
-    lidar_read(&vl53l1x);       // lowest priority - polls
+    // lidar_read(&vl53l1x);       // lowest priority - polls
 
     
 
@@ -143,24 +143,43 @@ static void LED_init(void) {
 
   // __HAL_RCC_GPIOC_CLK_ENABLE();
 
-  
+
   RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
   (void)RCC->AHBENR;
-  
+
   GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
                               GPIO_MODE_OUTPUT_PP,
                               GPIO_SPEED_FREQ_LOW,
                               GPIO_NOPULL};
 
   HAL_GPIO_Init(GPIOC, &initStr);
+  
+  // a real cute power on sequence
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_SET); // leds on
   HAL_Delay(1000);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // red off
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // green off
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7); // blue off
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8); // orange off
+  HAL_Delay(500);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, GPIO_PIN_SET); // leds on
+  HAL_Delay(500);
   HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // red off
   HAL_Delay(500);
   HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // green off
   HAL_Delay(500);
   HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7); // blue off
   HAL_Delay(500);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8); // orange off
+  HAL_Delay(500);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // red on
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // green on
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7); // blue on
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8); // orange on
+  HAL_Delay(1000);
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // red off
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9); // green off
+  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7); // blue off
   HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8); // orange off
   HAL_Delay(500);
 
