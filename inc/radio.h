23bf1e857e4e468293454ed96c974ec976455581
Gunnar Bostrom <u1586327@umail.utah.edu>
2026-04-16 12:00:41 -0600

    adding failsafe

--- inc/radio.h
+++ inc/radio.h
@@ -6,6 +6,8 @@ void radio_read(void);
 typedef struct {
     uint16_t throttle;  // channel 1, range 172-1811
     uint8_t armed;      // channel 5, 0 or 1
+    uint8_t  failsafe;  // 1 = signal lost
+
 } Radio_t;
 
 extern volatile Radio_t radio_data;
